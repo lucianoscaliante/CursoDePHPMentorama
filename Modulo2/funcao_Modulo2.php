61a5fff90f9e1eb59212f88c84949075f27aede9
lucianoscaliante <lucianoscaliante@gmail.com>
2022-08-08 20:04:55 -0300

    Atividade Modulo2 Função de multiplicação

---
+++ Modulo2/funcao_Modulo2.php
@@ -0,0 +1,14 @@
+<?php
+
+//Função para fazer a Multiplicação 
+function Multiplicacao($valor1, $valor2){
+    return $valor1 * $valor2;
+}
+
+//Corpo do codigo
+
+$resultado = Multiplicacao(2, 5);
+
+echo "Função de Multiplicação em PHP <br/>";
+echo "------------------------------<br/>";
+echo "O resultado da Multiplicação é: $resultado";
